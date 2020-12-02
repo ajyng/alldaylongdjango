ec8fad4e2afd385261f19248f05f6bf162de5838
ajyng <zxzimin@naver.com>
2020-12-02 18:16:32 +0900

    User, Post, Tag 모델 생성 및 등록

--- instagram/accounts/models.py
+++ instagram/accounts/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    class gender(models.TextChoices):
+        MALE = "M", "남성"
+        FEMALE = "F", "여성"
+    name = models.TextField()
+    follower_set = models.ManyToManyField("self", blank=True)
+    following_set = models.ManyToManyField("self", blank=True)
--- instagram/instagram/models.py
+++ instagram/instagram/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+class Post(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    photo = models.ImageField(upload_to="instagram/%Y/%m/%d")
+    caption = models.TextField(max_length=500)
+    tag_set = models.ManyToManyField('Tag', blank=True)
+
+    def __str__(self):
+        return self.caption
+
+class Tag(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+
+    def __str__(self):
+        return self.name

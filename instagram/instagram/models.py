04d7873a6f387876dba1f534cadd41482fea9ec5
ajyng <zxzimin@naver.com>
2020-12-04 19:01:19 +0900

    포스팅 쓰기 구현

--- instagram/instagram/models.py
+++ instagram/instagram/models.py
@@ -1,3 +1,4 @@
+import re
 from django.db import models
 from django.conf import settings
 
@@ -7,9 +8,22 @@ class Post(models.Model):
     photo = models.ImageField(upload_to="instagram/%Y/%m/%d")
     caption = models.TextField(max_length=500)
     tag_set = models.ManyToManyField('Tag', blank=True)
+    location = models.CharField(max_length=100)
 
     def __str__(self):
         return self.caption
+
+    def extract_tag_list(self):
+        tag_name_list = re.findall(r"#([a-zA-Z\dㄱ-힣]+)", self.caption)
+        tag_list = []
+        for tag_name in tag_name_list:
+            tag, _ = Tag.objects.get_or_create(name=tag_name)
+            tag_list.append(tag)
+        return tag_list
+
+    # def get_absolute_url(self):
+    #     return reverse("model_detail", kwargs={"pk": self.pk})
+    
 
 class Tag(models.Model):
     name = models.CharField(max_length=50, unique=True)
--- instagram/instagram/views.py
+++ instagram/instagram/views.py
@@ -1,4 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from .forms import PostForm
+from django.contrib import messages
+from .models import Tag
 
 def post_list(request):
     return render(request, 'instagram/post_list.html')
@@ -6,8 +10,25 @@ def post_list(request):
 def post_detail(request):
     pass
 
+@login_required
 def post_new(request):
-    pass
+    if request.method == 'POST':
+        form = PostForm(request.POST, request.FILES)
+        if form.is_valid():
+            post = form.save(commit=False) # get_absolute_url 활용 예정
+            post.author = request.user
+            post.save()
+            post.tag_set.add(*post.extract_tag_list()) # post.save()가 먼저 이뤄져야 한다. m2m 관계를 별도의 테이블에 저장하기 때문.
+
+            messages.success(request, "포스팅을 저장했습니다.")
+            return redirect("/")
+    else:
+        form = PostForm()
+    
+    return render(request, 'instagram/post_form.html', {
+        'form': form,
+    })
+    
 
 def post_update(request):
     pass

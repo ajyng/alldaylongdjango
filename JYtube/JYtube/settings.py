c847ab328607f12fda80cb285e9110e881315716
ajyng <zxzimin@naver.com>
2021-02-09 02:22:23 +0900

    Video 앱 생성

--- JYtube/JYtube/settings.py
+++ JYtube/JYtube/settings.py
@@ -31,6 +31,7 @@ INSTALLED_APPS = [
 
     # local apps
     'accounts',
+    'video',
 ]
 
 MIDDLEWARE = [
@@ -48,7 +49,9 @@ ROOT_URLCONF = 'JYtube.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR / 'templates',
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
